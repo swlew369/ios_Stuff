8de1f5c883e25cddaede731593084d2db5a46800
Scott Lew <scottlew@jows-mac-mini.home>
2017-05-05 09:19:53 -0400

    Initial Commit

---
+++ Slider1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slider1
+//
+//  Created by Scott Lew on 5/5/17.
+//  Copyright © 2017 Scott Lew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
