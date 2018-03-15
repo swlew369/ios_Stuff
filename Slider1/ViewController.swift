1628512fbb6dd64ec631c3d60240d80d71bdf3a0
Scott Lew <swl2265@gmail.com>
2018-03-15 11:02:12 -0400

    First commit to ios repository

--- Slider1/ViewController.swift
+++ Slider1/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TipSlider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +22,8 @@ class ViewController: UIViewController {
     }
 
 
-}
+    @IBAction func TipSliderPercentageValueChanged(_ sender: AnyObject) {
+        print(TipSlider.value)
+    }
+   }
 
